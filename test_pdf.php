db035db68d14f13e8bab811b2b5d322f49e5cb8c
Jade RIKAM <jade@jadis.lan>
2025-10-01 16:52:52 +0200

    Mise à jour des fichiers (ajouts, suppressions et modifs)

--- test_pdf.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php';
-
-$mpdf = new \Mpdf\Mpdf();
-$mpdf->WriteHTML('<h1>Test PDF</h1><p>Si tu vois ce PDF, tout fonctionne !</p>');
-$mpdf->Output();
